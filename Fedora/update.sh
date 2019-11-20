c176fb8fe1f7e54ae5a245e618093b369256dab7
damienhunter30 <damien.hunter9@gmail.com>
2019-11-20 12:03:15 +0000

    Update to update.sh

--- Fedora/update.sh
+++ Fedora/update.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
+
+echo "Starting System Update"
 sudo dnf update -y
 sudo dnf upgrade -y
+echo "Updates and Upgrades complete..."
+sleep(5000)
+
+echo "Removing unnecessary files...."
 sudo dnf autoremove -y
-echo "All Done!"
+echo "Files removed"
+sleep(5000)
+echo "Complete!"
