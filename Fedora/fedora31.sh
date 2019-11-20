02f9f3893ff2c3673c17a1058af74fff3caa5030
damienhunter30 <damien.hunter9@gmail.com>
2019-11-20 13:43:34 +0000

    Disclaimer added

--- Fedora/fedora31.sh
+++ Fedora/fedora31.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
+# Author Damien Hunter (damien.hunter9@gmail.com) 
+# (20/11/2019)
+#
 # Fedora Upgrade Tool. Upgrades to Fedora 31
+# Please note, I accept no liability for any problems
+# caused by this script, as I have ran it successfully.
 
 sudo dnf upgrade --refresh -y
 sudo dnf install dnf-plugin-system-upgrade -y
