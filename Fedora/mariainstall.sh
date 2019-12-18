8ce9ab89597ec34fd39521059b8294b1c89f5113
Damien <damien.hunter9@gmail.com>
2019-12-18 13:20:25 +0000

    Mariadb Install Script added

---
+++ Fedora/mariainstall.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# Install and Start MariaDB. It must be set up and run with sudo. You
+# run this at your own risk.
+#
+# Damien Hunter damien.hunter9@gmail.com
+# See https://www.if-not-true-then-false.com/2013/install-mariadb-on-fedora-centos-rhel/
+# for full set up and configuration notes.
+
+sudo dnf install mariadb mariadb-server # Install Mariadb
+systemctl enable mariadb.service # Enable and Start Mariadb
+systemctl start mariadb.service
+
+/usr/bin/mysql_secure_installation # Run the installation and set root password
+
+# If you want to add the ability to connect remotely, see the link above.
