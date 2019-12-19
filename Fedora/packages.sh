e12110c57aaecfda9754c1a2082e5f2aa42d9b64
Damien <damien.hunter9@gmail.com>
2019-12-19 23:02:48 +0000

    Packages Tidy Up

--- Fedora/packages.sh
+++ Fedora/packages.sh
@@ -1,35 +1,15 @@
 #!/bin/bash
 
 # Packages Installer
-
-# Install Fedy to run Desktop Applications with a better experience
-sudo dnf update
+sudo dnf update -y
 
 # Other Packages
-sudo dnf install -y vlc				# Install VLC
-sudo dnf install -y unrar p7zip			# Archive Tools
-sudo dnf install -y nano			# Install Nano Editor
-sudo dnf install -y vim				# Install VIM Editor
-sudo dnf install -y unzip			# Archive Tool
-sudo dnf install -y icedtea-web java-openjdk	# Other Java Tools
-sudo dnf install -y clamav			# Anti-Virus
-sudo dnf install -y clamtv			# ClamAV GUI
-sudo dnf install -y vagrant
-sudo dnf install -y nginx			# Web Server
-sudo dnf install -y audacious
-sudo dnf install -y audacity
-sudo dnf install -y gnome-music
-sudo dnf install -y qbittorrent			# Torrent Downloader
+sudo dnf install -y vlc	p7zip vim VirtualBox
+sudo dnf install -y unzip icedtea-web java-openjdk
+sudo dnf install -y clamav vagrantvnginx audacious audacity
 
 # Install a new Repository
 sudo dnf install --nogpgcheck   http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-
-# Desktop Environments
-sudo dnf install @kde-desktop -y		# KDE
-sudo dnf install @xfce-desktop -y		# XFCE
-sudo dnf install @lxde-desktop -y		# LXDE
-sudo dnf install @cinnamon-desktop -y		# Cinnamon
-sudo dnf -y group install "KDE Plasma Workspaces"	#KDE Group Install
 
 # Flash
 sudo rpm -ivh http://linuxdownload.adobe.com/adobe-release/adobe-release-i386-1.0-1.noarch.rpm
@@ -37,14 +17,10 @@ sudo rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-adobe-linux
 sudo dnf install flash-plugin -y
 
 # More
-sudo dnf install gnome-tweak-tool gimp inkscape thunderbird bluefish filezilla vlc python-openoffice -y
-sudo dnf install VirtualBox -y
-sudo dnf install dconf-editor -y
-sudo dnf install epiphany -y
-sudo dnf install gnome-tweak-tool -y
+sudo dnf install gnome-tweak-tool gimp inkscape thunderbird bluefish filezilla vlc python-openoffice dconf-editor epiphany gnome-tweak-tool -y
 
 # Codecs
-sudo dnf install gstreamer-plugins-bad gstreamer-plugins-bad-free-extras  gstreamer-plugins-ugly gstreamer-ffmpeg gstreamer1-libav gstreamer1-plugins-bad-free-extras gstreamer1-plugins-bad-freeworld gstreamer1-plugins-base-tools gstreamer1-plugins-good-extras gstreamer1-plugins-ugly gstreamer1-plugins-bad-free gstreamer1-plugins-good gstreamer1-plugins-base gstreamer1 -y
+sudo dnf install gstreamer gstreamer-plugins-ugly gstreamer-ffmpeg gstreamer1-libav gstreamer1-plugins-bad-freeworld gstreamer1-plugins-base-tools gstreamer1-plugins-good-extras gstreamer1-plugins-good gstreamer1-plugins-base gstreamer1 -y
 
 # Remove
 sudo dnf remove evolution rhythmbox empathy gnome-maps -y
