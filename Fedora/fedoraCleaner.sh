d4b6b47b037f6f038f6b640645cdf864465c3bc2
Damien <damien.hunter9@gmail.com>
2020-01-15 11:06:12 +0000

    Updated the fedoraCleaner.sh script

--- Fedora/fedoraCleaner.sh
+++ Fedora/fedoraCleaner.sh
@@ -5,8 +5,9 @@
 #
 # Damien Hunter (damien.hunter9@gmail.com) 26/12/2015
 
-sudo dnf update -y
-sudo dnf clean all
+sudo dnf update --best --allowerasing -y
+sudo dnf clean all -y
+sudo dnf autoremove -y
 
 echo "Remove unnecessary log files!"
 sudo find / -name *.log -type f -delete
