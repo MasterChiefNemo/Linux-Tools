b9a489c2ab05815de25510e5eade0fa8eaa76288
Damien <damien.hunter9@gmail.com>
2020-04-06 21:59:52 +0100

    modified:   docker.sh

--- Fedora/docker.sh
+++ Fedora/docker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo dnf update -y && sudo dnf upgrade -y
+
 sudo dnf config-manager --add-repo=https://download.docker.com/linux/fedora/docker-ce.repo
 sudo dnf install docker-ce docker-compose -y
 sudo systemctl enable --now docker
