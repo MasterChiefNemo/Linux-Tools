896f8d66a621bf3a37dbccd6eb1c68e6b63fd313
damienhunter30 <damien.hunter9@gmail.com>
2020-07-19 23:34:18 +0100

    Cleaner update

--- Debian/debianCleaner.sh
+++ Debian/debianCleaner.sh
@@ -4,34 +4,28 @@
 # Debian based distributions.
 #
 # Damien HUnter (damien.hunter30@gmail.com)
-echo "Performing an Update and Upgrade!"
-sleep 5
-apt-get -y update # System Update
-apt-get -y upgrade # System Upgrade
-apt-get -y dist-upgrade
 
-echo
+echo "Performing an Update and Upgrade!"
+sleep 2
+sudo apt-get -y update # System Update
+sudo apt-get -y upgrade # System Upgrade
+sudo apt-get -y dist-upgrade
+
 echo "Performing a clean up operation!"
-sleep 5
-apt-get remove
-apt-get -y  autoremove # Removes all non-essential packages.
-echo
-apt-get clean # Clean up.
-echo
-apt-get autoclean # Additional clean up.
-echo
+sleep 2
+sudo apt-get remove
+sudo apt-get -y  autoremove # Removes all non-essential packages.
+sudo apt-get clean # Clean up.
+sudo apt-get autoclean # Additional clean up.
 echo "Done!"
 
-# This shell script finds and removes all bak and log files.
-# There's a C file to do this, but if you fancy a Shell script
-# instead, here it is.
-#
-# Damien Hunter (damien.hunter9@gmail.com) 26/12/2015
+sleep 3
+
 echo "Remove unnecessary log files!"
-find / -name *.log -type f -delete
-sleep 5
+sudo find / -name *.log -type f -delete
+sleep 2
 
 echo "Remove unnecessary bak files!"
-find / -name *.log -type f -delete
+sudo find / -name *.log -type f -delete
 echo "Done!"
-sleep 5
+sleep 2
