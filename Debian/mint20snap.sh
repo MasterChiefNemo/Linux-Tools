b6e63a500259bdd1691a60c2749b866518e10736
damienhunter30 <damien.hunter9@gmail.com>
2020-07-19 23:41:37 +0100

    Updated Snap installer for Mint 20

--- Debian/mint20snap.sh
+++ Debian/mint20snap.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# Installs snap for Linux Mint 20, allowing users to download apps
+# from https://snapcraft.io/
+#
+# Author: Damien Hunter (damien.hunter9@gmail.com)
+
 sudo mv /etc/apt/preferences.d/nosnap.pref ~
 sudo apt install snapd
