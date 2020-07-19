2b7ccdb3de22ce2bf7a40803441243956e8b802c
damienhunter30 <damien.hunter9@gmail.com>
2020-07-19 23:38:32 +0100

    Script Update

--- Debian/system_update.sh
+++ Debian/system_update.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-# Script to quickly update the VM.
+# Script to quickly update Debian based systems.
+# Author: Damien Hunter (damien.hunter9@gmail.com)
+echo "Starting System Update..."
 sudo apt-get update -y && sudo apt-get upgrade -y
+echo
+
 sudo apt-get autoclean -y
+echo
+
 sudo apt-get autoremove -y
+echo
+echo "Done"
