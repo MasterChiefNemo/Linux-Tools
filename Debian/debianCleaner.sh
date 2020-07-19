a558efa2fe8254a2b09b9941c63473c0bd91c95b
damienhunter30 <damien.hunter9@gmail.com>
2020-07-19 23:35:49 +0100

    Cleaner update

--- Debian/debianCleaner.sh
+++ Debian/debianCleaner.sh
@@ -10,6 +10,7 @@ sleep 2
 sudo apt-get -y update # System Update
 sudo apt-get -y upgrade # System Upgrade
 sudo apt-get -y dist-upgrade
+echo
 
 echo "Performing a clean up operation!"
 sleep 2
@@ -18,14 +19,15 @@ sudo apt-get -y  autoremove # Removes all non-essential packages.
 sudo apt-get clean # Clean up.
 sudo apt-get autoclean # Additional clean up.
 echo "Done!"
+echo
 
 sleep 3
 
 echo "Remove unnecessary log files!"
 sudo find / -name *.log -type f -delete
 sleep 2
+echo
 
 echo "Remove unnecessary bak files!"
 sudo find / -name *.log -type f -delete
 echo "Done!"
-sleep 2
