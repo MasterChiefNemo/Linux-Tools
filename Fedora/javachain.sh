1624702573aa37ac5b7a515c8b8e79d42c62a9c2
Damien Hunter <a00014290@student.ait.ie>
2021-01-22 13:25:52 +0000

    New Script Added

---
+++ Fedora/javachain.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sudo date;./minifab install -c mychannel -l java -n abstore -v 3.1 -p '"init","a","231","b","331"'
+sudo date;./minifab approve,commit; date
+sudo date; ./minifab initialize; date
+sudo date;./minifab invoke; date
+sudo date;./minifab invoke -p '"query","a"'; date
+sudo date;./minifab invoke -p '"query","b"'; date
