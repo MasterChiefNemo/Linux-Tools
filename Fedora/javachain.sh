83fe36bf4cbe221cc6278140ae88bd664577d66e
root <root@centos.europe-west2-a.c.sigma-zodiac-302313.internal>
2021-01-28 21:25:12 +0000

    Java Chain Code Update

--- Fedora/javachain.sh
+++ Fedora/javachain.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+cd ../..
+
+sudo git clone https://github.com/hyperledger-labs/minifabric.git
+
+cd minifabric
+
 sudo date;./minifab install -c mychannel -l java -n abstore -v 3.1 -p '"init","a","231","b","331"'
 sudo date;./minifab approve,commit; date
 sudo date; ./minifab initialize; date
