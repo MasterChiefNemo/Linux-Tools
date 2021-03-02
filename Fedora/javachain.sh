de0b762913252da70e4d64a1489fd94e0a15b20d
Damien Hunter <a00014290@student.ait.ie>
2021-03-02 21:32:26 +0000

    Update javachain.sh

--- Fedora/javachain.sh
+++ Fedora/javachain.sh
@@ -6,9 +6,9 @@ sudo git clone https://github.com/hyperledger-labs/minifabric.git
 
 cd minifabric
 
-sudo date;./minifab install -c mychannel -l java -n abstore -v 3.1 -p '"init","a","231","b","331"'
-sudo date;./minifab approve,commit; date
-sudo date; ./minifab initialize; date
-sudo date;./minifab invoke; date
-sudo date;./minifab invoke -p '"query","a"'; date
-sudo date;./minifab invoke -p '"query","b"'; date
+sudo ./minifab install -c mychannel -l java -n abstore -v 3.1 -p '"init","a","231","b","331"'
+sudo ./minifab approve,commit
+sudo ./minifab initialize
+sudo ./minifab invoke
+sudo ./minifab invoke -p '"query","a"'
+sudo ./minifab invoke -p '"query","b"'
