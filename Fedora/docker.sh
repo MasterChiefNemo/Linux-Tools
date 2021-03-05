60d5f5b4af64dba5a7f1b9be776ccf960ba5f2ae
Damien Hunter <a00014290@student.ait.ie>
2021-03-05 13:45:31 +0000

    Update docker.sh

--- Fedora/docker.sh
+++ Fedora/docker.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+sudo dnf install curl -y
 
-sudo dnf update -y && sudo dnf upgrade -y
+sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+sudo dnf install docker-ce --nobest -y
+sudo systemctl enable docker
+sudo systemctl start docker
 
-sudo dnf config-manager --add-repo=https://download.docker.com/linux/fedora/docker-ce.repo
-sudo dnf install docker docker-ce docker-compose -y --allowerasing
-sudo systemctl enable --now docker
-sudo systemctl status docker
-sudo groupadd docker
+sudo curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo chmod 755 /usr/local/bin/docker-compose
