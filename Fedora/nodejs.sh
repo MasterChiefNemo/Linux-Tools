52414633d324af4dea9f1af24b0a7b50bb7682fa
root <root@mx>
2021-09-23 13:55:08 -0400

    new file:   nodejs.sh

---
+++ Fedora/nodejs.sh
@@ -0,0 +1,10 @@
+!#/bin/bash
+
+sudo dnf install gcc-c++ make -y
+sudo curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
+sudo dnf install nodejs -y
+
+sudo curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
+sudo rpm --import https://dl.yarnpkg.com/rpm/pubkey.gpg
+
+sudo dnf install yarn -y
