74ac5549ecd682f7817e3c126ee756c88efffc03
root <root@test-box.us-west4-b.c.coastal-haven-326111.internal>
2021-09-25 13:51:28 +0000

    modified:   nodejs.sh

--- Fedora/nodejs.sh
+++ Fedora/nodejs.sh
@@ -1,7 +1,7 @@
 !#/bin/bash
 
 sudo dnf install gcc-c++ make -y
-sudo curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
+sudo curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
 sudo dnf install nodejs -y
 
 sudo curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
