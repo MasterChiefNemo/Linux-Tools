3c735c3c7ebdde77be0e2b51ef8e2c8e7e9c4366
Damien Hunter <damien.hunter9@gmail.com>
2023-08-18 09:52:49 +0100

    modified:   fedora33.sh

--- Fedora/fedora33.sh
+++ Fedora/fedora33.sh
@@ -3,13 +3,15 @@
 # Author Damien Hunter (damien.hunter9@gmail.com)
 # (26/04/2020)
 #
-# Fedora Upgrade Tool. Upgrades to Fedora 32
+# Fedora Upgrade Tool. Upgrades to Fedora 38
 # Please note, I accept no liability for any problems
 # caused by this script, as I have ran it successfully.
+#
+# Updated: August 2023
 #
 # Damien Hunter damien.hunter9@gmail.com
 
 sudo dnf upgrade --refresh -y
 sudo dnf install dnf-plugin-system-upgrade -y
-sudo dnf system-upgrade download --releasever=33 -y
+sudo dnf system-upgrade download --releasever=38 -y
 sudo dnf system-upgrade reboot
