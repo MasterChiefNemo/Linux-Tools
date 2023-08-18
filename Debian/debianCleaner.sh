da9e922961d2654bc49428d81aa06e3dc90a8f85
Damien Hunter <damien.hunter9@gmail.com>
2023-08-18 09:37:10 +0100

    modified:   debianCleaner.sh

--- Debian/debianCleaner.sh
+++ Debian/debianCleaner.sh
@@ -7,17 +7,15 @@
 
 echo "Performing an Update and Upgrade!"
 sleep 2
-sudo apt-get -y update # System Update
-sudo apt-get -y upgrade # System Upgrade
-sudo apt-get -y dist-upgrade
+sudo apt update -y && sudo apt upgrade -y # System Update & Upgrade
+sudo apt-get dist-upgrade -y
 echo
 
 echo "Performing a clean up operation!"
 sleep 2
-sudo apt-get remove
-sudo apt-get -y  autoremove # Removes all non-essential packages.
-sudo apt-get clean # Clean up.
-sudo apt-get autoclean # Additional clean up.
+sudo apt remove
+sudo apt autoremove -y # Removes all non-essential packages.
+sudo apt autoclean # Additional clean up.
 echo "Done!"
 echo
 
@@ -25,7 +23,6 @@ sleep 3
 
 echo "Remove unnecessary log files!"
 sudo find / -name *.log -type f -delete
-sleep 2
 echo
 
 echo "Remove unnecessary bak files!"
