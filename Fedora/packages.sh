c3884993c533b186df7c2e58ead3264af93c958f
Damien Hunter <damien.hunter9@gmail.com>
2023-08-21 09:02:37 +0100

    modified:   packages.sh

--- Fedora/packages.sh
+++ Fedora/packages.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Packages Installer
-sudo dnf update -y
+sudo dnf update -y && sudo dnf upgrade -y
 
 # Other Packages
 sudo dnf install -y vlc	p7zip VirtualBox
 sudo dnf install -y unzip java-openjdk
-sudo dnf install -y clamav vagrant nginx
+sudo dnf install -y clamav vagrant nginx httpd
 
 # Install a new Repository
 sudo dnf install --nogpgcheck http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
@@ -14,7 +14,6 @@ sudo dnf install --nogpgcheck http://download1.rpmfusion.org/free/fedora/rpmfusi
 # Flash
 sudo rpm -ivh http://linuxdownload.adobe.com/adobe-release/adobe-release-i386-1.0-1.noarch.rpm
 sudo rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-adobe-linux
-sudo dnf install flash-plugin -y
 
 # More
 sudo dnf install gnome-tweak-tool gimp bluefish filezilla vlc dconf-editor epiphany -y
