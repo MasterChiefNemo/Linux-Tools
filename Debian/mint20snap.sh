e8eefb66a35cb9ea9ac7ee5f8c07818bc8d2df8d
Damien Hunter <damien.hunter9@gmail.com>
2023-08-23 12:38:20 +0100

    modified:   mint20snap.sh

--- Debian/mint20snap.sh
+++ Debian/mint20snap.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 # Installs snap for Linux Mint 20, allowing users to download apps
-# from https://snapcraft.io/
+# from https://snapcraft.io/ in the command line. To search for snaps to install
+# in the command line, use
 #
-# Author: Damien Hunter (damien.hunter9@gmail.com)
+# 'sudo snap find search_term'
+#
+# Author: Damien Hunter damien.hunter9@gmail.com
+# Updated, August 2023
 
 sudo mv /etc/apt/preferences.d/nosnap.pref ~
 sudo apt install snapd
