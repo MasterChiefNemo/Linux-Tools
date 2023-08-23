b4fbb692ff8ae803f48691a33879297d173fd298
Damien Hunter <damien.hunter9@gmail.com>
2023-08-23 12:42:27 +0100

    modified:   mariadb.sh

--- Debian/mariadb.sh
+++ Debian/mariadb.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
+#
+# Retrieves and installs Mariadb
+#
+# Damien Hunter damien.hunter9@gmail.com
+#
+# Updated: August 2023
+#
 
-sudo apt-get install -y software-properties-common
+sudo apt install software-properties-common -y
 sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
 sudo mv mariadb.list /etc/apt/sources.list.d/mariadb.list
 sudo apt update -y
